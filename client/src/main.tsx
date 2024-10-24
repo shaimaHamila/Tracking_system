3cc5675648453b059552f5c351db119d72b969af
shaimaHamila <hamilachaima18@gmail.com>
2024-10-24 22:19:58 +0100

    Remove redux from project

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,18 +5,14 @@ import App from "./App.tsx";
 import "./main.scss";
 import "./styles/typography/headings.scss";
 import "./styles/colors/colors.scss";
-import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
 import { ConfigProvider } from "antd";
 import { antdThemeConfig } from "./styles/antdThemeConfig/antdThemeConfig.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ConfigProvider theme={antdThemeConfig}>
-          <App />
-        </ConfigProvider>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <ConfigProvider theme={antdThemeConfig}>
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
