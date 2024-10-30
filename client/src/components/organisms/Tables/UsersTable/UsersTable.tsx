f930cdd9cab7ca365fd7edbf50d2fb71f00b01ba
shaimaHamila <hamilachaima18@gmail.com>
2024-10-30 21:28:34 +0100

    User table design integration

--- client/src/components/organisms/Tables/UsersTable/UsersTable.tsx
+++ client/src/components/organisms/Tables/UsersTable/UsersTable.tsx
@@ -1,101 +1,219 @@
-import React from "react";
-import { Space, Table } from "antd";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Button, Pagination, Popconfirm, Space, Table, Tag, Tooltip } from "antd";
 import type { TableProps } from "antd";
 import TableHeader from "../../Headers/TableHeader/TableHeader";
-import { Role } from "../../../../types/Role";
+import { Role, RoleName } from "../../../../types/Role";
 import { Project } from "../../../../types/Project";
+import { HiOutlineEye, HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi";
+import { User } from "../../../../types/User";
 
-interface DataType {
-  key: string;
+interface UserTableRow {
+  id: number;
   name: string;
   email: string;
   role: Role;
   projects: Partial<Project>[];
+  user: User;
+}
+interface UserTableProps {
+  users: User[];
+  status: "idle" | "loading" | "failed";
+  totalUsers: number;
+  onViewUser: (user: User) => void;
+  onUpdateUser: (user: User) => void;
+  onDeleteUser: (id: string) => void;
+  limitUsersPerPage: number;
+  onPageChange: (page: number) => void;
+  handlePageSizeChange: (current: number, pageSize: number) => void;
+  addNewUser: () => void;
+  addBtnText: string;
+  onSearchChange: (searchedName: string) => void;
 }
 
-const columns: TableProps<DataType>["columns"] = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Role",
-    dataIndex: "role",
-    key: "role",
-  },
-  {
-    title: "Projects",
-    key: "projects",
-    dataIndex: "projects",
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: () => (
-      <Space size='middle'>
-        <a>View</a>
-        <a>Edit</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+const UserTable: React.FC<UserTableProps> = ({
+  users,
+  totalUsers,
+  onDeleteUser,
+  onUpdateUser,
+  onViewUser,
+  onPageChange,
+  onSearchChange,
+  addBtnText,
+  addNewUser,
+  handlePageSizeChange,
+  limitUsersPerPage,
+  status,
+}) => {
+  const [pageSize, setPageSize] = useState<number>(limitUsersPerPage);
+  const [tableContent, setTableContent] = useState<UserTableRow[]>([]);
 
-const UserTable: React.FC = () => {
+  useEffect(() => {
+    // Extract specific fields from orders and populate tableContent
+    const _tableContent = users?.map((user) => ({
+      id: user?.id!,
+      name: `${user?.firstName} ${user?.lastName}`,
+      email: user?.email,
+      role: user?.role,
+      projects: user?.projects,
+      user: user,
+    }));
+    setTableContent(_tableContent);
+  }, [users]);
+
+  const columns: TableProps<UserTableRow>["columns"] = [
+    {
+      title: "",
+      key: "number",
+      width: 50,
+      render: (_, _record, index) => index + 1,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "role",
+      key: "role",
+      render: (role) => {
+        let tagColor;
+        switch (role.roleName) {
+          // case RoleName.ADMIN:
+          //   tagColor = "volcano";
+          //   break;
+          case RoleName.STAFF:
+            tagColor = "processing";
+            break;
+          // case RoleName.CLIENT:
+          //   tagColor = "gold";
+          //   break;
+          // case RoleName.TECHNICAL_MANAGER:
+          //   tagColor = "purple";
+          //   break;
+          default:
+            tagColor = "default";
+        }
+        return <Tag color={tagColor}>{role.roleName}</Tag>;
+      },
+    },
+    {
+      title: "Projects",
+      key: "projects",
+      dataIndex: "projects",
+      width: "30%",
+      render: (projects) => {
+        const displayedProjects = projects.slice(0, 3);
+        const remainingProjects = projects.length - displayedProjects.length;
+
+        return (
+          <div>
+            {displayedProjects.map((project: Partial<Project>) => (
+              <Tag bordered={false} key={project.id} color='default' style={{ marginBottom: "5px" }}>
+                {project.name}
+              </Tag>
+            ))}
+            {remainingProjects > 0 && (
+              <Tooltip title={projects.map((project: Partial<Project>) => project.name).join(", ")}>
+                <Tag bordered={false} color='default'>
+                  +{remainingProjects} more
+                </Tag>
+              </Tooltip>
+            )}
+          </div>
+        );
+      },
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: (user) => (
+        <Space size='middle'>
+          <Tooltip title='View'>
+            <Button
+              onClick={() => {
+                onViewUser(user?.user);
+              }}
+              className='table--action-btn'
+              icon={<HiOutlineEye />}
+            />
+          </Tooltip>
+          <Tooltip title='Edit'>
+            <Button
+              onClick={() => {
+                onUpdateUser(user?.user);
+              }}
+              className='table--action-btn'
+              icon={<HiOutlinePencilAlt />}
+            />
+          </Tooltip>
+
+          <Tooltip title='Delete'>
+            <Popconfirm
+              title='Are you sur you want to delete this user?'
+              onConfirm={() => {
+                // store.dispatch(setLoading(true));
+                onDeleteUser(user.id!);
+              }}
+            >
+              <Button className='table--action-btn' icon={<HiOutlineTrash />} loading={false} />
+            </Popconfirm>
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
+  const onPageSizeChange = (_current: number, size: number) => {
+    setPageSize(size);
+    handlePageSizeChange(_current, size);
+  };
+  const [tableHeight, setTableHeight] = useState(300);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      const { top } = ref.current.getBoundingClientRect();
+      // Adjust TABLE_HEADER_HEIGHT according to your actual header height.
+      const TABLE_HEADER_HEIGHT = 55;
+      setTableHeight(window.innerHeight - top - TABLE_HEADER_HEIGHT - 100);
+    }
+  }, [ref]);
   return (
-    <div>
+    <div ref={ref} style={{ height: "100%", overflow: "auto" }}>
       <TableHeader
-        onSearchChange={(searchedId) => console.log(searchedId)}
-        onClickBtn={() => console.log("clicked")}
-        btnText={"Add user"}
-        totalItems={10}
+        onSearchChange={(searchedName) => onSearchChange(searchedName)}
+        onClickBtn={addNewUser}
+        btnText={addBtnText}
+        totalItems={totalUsers}
         totalItemsText={"Total users:"}
         searchPlaceholder={"Search by name"}
       />
-      <Table<DataType> columns={columns} dataSource={data} />
+      <Table<UserTableRow>
+        loading={status == "loading"}
+        rowKey='id'
+        columns={columns}
+        dataSource={tableContent}
+        pagination={false}
+        scroll={{ y: tableHeight, x: 700 }}
+      />
+      <Pagination
+        style={{ margin: "26px", textAlign: "right", justifyContent: "flex-end" }}
+        total={totalUsers}
+        pageSize={pageSize}
+        showSizeChanger
+        showTotal={(total, range) => `${range[0]}-${range[1]} ${"of"} ${total} ${"Users"}`}
+        onChange={onPageChange}
+        onShowSizeChange={onPageSizeChange}
+      />
     </div>
   );
 };
 
 export default UserTable;
-
-const data: DataType[] = [
-  {
-    key: "1",
-    name: "John Brown",
-    email: "test@gmail.com",
-    role: {
-      id: 2,
-      roleName: "ADMIN",
-    },
-    projects: [],
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    email: "test@gmail.com",
-
-    role: {
-      id: 3,
-      roleName: "STAFF",
-    },
-    projects: [],
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    email: "test@gmail.com",
-    role: {
-      id: 4,
-      roleName: "CLIENT",
-    },
-    projects: [],
-  },
-];
--- client/src/pages/Users/Users.tsx
+++ client/src/pages/Users/Users.tsx
@@ -1,32 +1,103 @@
 import { Tabs, TabsProps } from "antd";
 import { LiaUser, LiaUserCogSolid, LiaUserTieSolid } from "react-icons/lia";
 import UserTable from "../../components/organisms/Tables/UsersTable/UsersTable";
+import { User } from "../../types/User";
 const Users = () => {
   const onChange = (key: string) => {
     console.log(key);
   };
+  const renderUserTable = (addBtnText: string) => (
+    <UserTable
+      users={data}
+      status={"idle"}
+      totalUsers={0}
+      onViewUser={(user: User) => console.log(user)}
+      onUpdateUser={(user: User) => console.log(user)}
+      onDeleteUser={(id: string) => console.log(id)}
+      limitUsersPerPage={10}
+      onPageChange={(page: number) => console.log(page)}
+      handlePageSizeChange={(current: number, pageSize) => console.log(current, pageSize)}
+      addNewUser={() => console.log(`Add new ${addBtnText.toLowerCase()}`)}
+      addBtnText={addBtnText}
+      onSearchChange={(searchedName: string) => console.log(searchedName)}
+    />
+  );
 
   const items: TabsProps["items"] = [
     {
       key: "1",
       label: "Staff",
-      children: <UserTable />,
       icon: <LiaUser />,
+      children: renderUserTable("Add new staff"),
     },
     {
       key: "2",
       label: "Clients",
-      children: "Clients",
       icon: <LiaUserTieSolid />,
+      children: renderUserTable("Add new client"),
     },
     {
       key: "3",
       label: "Tech Managers",
-      children: "Tech Managers",
       icon: <LiaUserCogSolid />,
+      children: renderUserTable("Add new tech manager"),
+    },
+    {
+      key: "4",
+      label: "Admins",
+      icon: <LiaUserCogSolid />,
+      children: renderUserTable("Add new admin"),
     },
   ];
   return <Tabs defaultActiveKey='1' items={items} onChange={onChange} />;
 };
 
 export default Users;
+
+const data: any[] = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Brown",
+    email: "test@gmail.com",
+    role: {
+      id: 2,
+      roleName: "ADMIN",
+    },
+    projects: [{ id: 1, name: "Project 1" }],
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Brown",
+    email: "test@gmail.com",
+
+    role: {
+      id: 3,
+      roleName: "STAFF",
+    },
+    projects: [
+      { id: 1, name: "Project 1" },
+      { id: 2, name: "Project 2" },
+      { id: 3, name: "Project 3" },
+      { id: 4, name: "Project 4" },
+      { id: 4, name: "Project 5" },
+      { id: 4, name: "Project 6" },
+      { id: 4, name: "Project 7" },
+    ],
+  },
+  {
+    id: 3,
+    firstName: "John",
+    lastName: "Brown",
+    email: "test@gmail.com",
+    role: {
+      id: 4,
+      roleName: "CLIENT",
+    },
+    projects: [
+      { id: 1, name: "Project 1" },
+      { id: 2, name: "Project 2" },
+    ],
+  },
+];
